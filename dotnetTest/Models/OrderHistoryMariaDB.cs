a848c5c12802e23b73611d730a67ccaaa8bfde46
teddy0 thinkpad laptop <fifadude114@gmail.com>
2022-03-31 14:57:00 -0700

    renamed OrderHistory to OrderHistoryRAM, and also added a class for OrderHistory implementation using MariaDB entity framework core

---
+++ dotnetTest/Models/OrderHistoryMariaDB.cs
@@ -0,0 +1,181 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace dotnetTest.Models;
+
+// here's an interface for some of our data models
+// we can use databases later
+
+public class OrderHistoryMariaDB : IOrderHistory 
+{
+
+	// to implement the interface, we first
+	// declare a private list variable
+	// lists implement the IEnumerable interface
+	// so we don't have to implement them 
+	
+	
+        private List<IOrder> _orderHistory;
+
+	public OrderHistoryMariaDB(){
+
+		// in the constructor, we initiate a new list
+		// a list is an implementation of the IEnumerable interface
+		// and the Order is an implementation of IOrder
+		// no dependency injection here, it's pretty tightly 
+		// coupled
+		//
+		// i choose not to because i will then have to register
+		// not just the orderhistory service but the order service
+
+		_orderHistory = new List<IOrder>();
+
+
+	}
+
+	// for order creation, we just use the add function within the list
+
+	public void createOrder(IOrder order){
+
+		// first i need a function to find the max id in my 
+		// _orderHistory
+		//
+
+		int maxID;
+		int nextID;
+
+		maxID = _orderHistory.Count;
+
+		nextID = maxID + 1;
+
+		order.id = nextID;	
+
+		_orderHistory.Add(order);
+
+	}
+
+	
+
+	// for order deletion, the implementation is similar
+	// we usually use the remove method in the list
+	// however, the remove method requires an object 
+	// first in order to know what to remove
+	// so we need a get method to retrieve the correct object for removal
+	//
+	
+	public IOrder getOrder(int id){
+
+		IOrder order;
+		
+		order = _orderHistory.FirstOrDefault(Order => Order.id == id);	
+
+		// first or default returns the first or default object
+		// , in the above case
+		// Order
+		// if the object satisfies a condition
+		// this is denoted with
+		// => 
+		// which represents an if statement
+
+
+	        return order;	
+
+	}
+	
+	public IOrder getOrder(string customer){
+
+		IOrder order;
+		
+		order = _orderHistory.FirstOrDefault(Order => Order.customer
+			       	== customer);	
+	        return order;	
+
+	}
+
+	// after implementing both get functions
+	// we can then use it to retrieve the correct order for deletion
+	
+
+	public void deleteOrder(int id){
+
+		IOrder order;
+
+
+		if(this.getOrder(id) != null){
+
+		order = this.getOrder(id);
+
+		_orderHistory.Remove(order);
+		}
+
+	}
+	
+
+	public void deleteOrder(string customer){
+
+		IOrder order;
+
+		order = this.getOrder(customer);
+
+		_orderHistory.Remove(order);
+
+	}
+
+	// now that deletion is complete, we can then use those methods to update
+	// order
+	//
+	//
+	//
+	
+	public void clearAllOrders(){
+
+		_orderHistory.Clear();
+
+	}
+	
+	public void updateOrder(IOrder order, int id){
+
+		// first i will delete order by id
+
+		this.deleteOrder(id);
+
+		// second i will ensure that the order id is equal
+		// to the id given (this is more a failsafe)
+
+		order.id = id;
+
+		// third i will add the order back to the list
+		//
+
+		_orderHistory.Add(order);
+
+	}
+	public void updateOrder(IOrder order, string customer){
+
+		// first i will delete order by id
+
+		this.deleteOrder(customer);
+
+		// second i will ensure that the order id is equal
+		// to the id given (this is more a failsafe)
+
+		order.customer = customer;
+
+		// third i will add the order back to the list
+		//
+
+		_orderHistory.Add(order);
+
+	}
+
+	public IEnumerable<IOrder> getOrderHistory(){
+
+		return _orderHistory;
+	}	
+
+
+
+
+
+}
+
