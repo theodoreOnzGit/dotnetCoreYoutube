7a79676740354defb2ee66e4d246ddba03d125c6
teddy0 thinkpad laptop <fifadude114@gmail.com>
2022-04-05 16:42:23 -0700

    new dataStorage page added, ready for video

---
+++ dotnetTest/Pages/dataStorage.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnetTest.Pages;
+
+public class dataStorageModel : PageModel
+{
+    private readonly ILogger<dataStorageModel> _logger;
+
+    public dataStorageModel(ILogger<dataStorageModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+
+    }
+}
